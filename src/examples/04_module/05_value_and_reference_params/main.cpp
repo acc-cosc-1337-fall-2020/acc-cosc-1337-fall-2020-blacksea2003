d07b39320b7e70c1e37370f8de6b23ddab8d46c3
blacksea2003 <vaibhavkrishnaswamy@gmail.com>
2020-09-29 17:38:14 -0500

    new

--- src/examples/04_module/05_value_and_reference_params/main.cpp
+++ src/examples/04_module/05_value_and_reference_params/main.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
+#include "value_ref.h"
+
+using std::cout; using std::cin;
 
 int main() 
 {
+	/*
+	int num = 5;
+
+	cout<<"value of num: "<<num<<"\n";
+	cout<<"stack adress of num: "<<&num<<"\n";
+
+	int& num_ref = num;
+
+	cout<<"value of num_ref: "<<num_ref<<"\n";
+	cout<<"stack adress of num_ref: "<<&num_ref<<"\n";
+
+	num_ref = 10;
+	cout<<"value of num: "<<num<<"\n";
+	cout<<"value of num_ref: "<<num_ref<<"\n";
+*/
+	void sample_static_var();
+	void sample_static_var();
+	void sample_static_var();
+	
 
 	return 0;
 }
--- src/examples/04_module/05_value_and_reference_params/value_ref.cpp
+++ src/examples/04_module/05_value_and_reference_params/value_ref.cpp
@@ -1,5 +1,5 @@
 #include "value_ref.h"
-
+#include<iostream>
 int get_area(int height, int width)
 {
 return height*width;
@@ -15,7 +15,12 @@ The function assigns 20 to num1 and 50 to num2
 
 @return does not return a value
 */
+void pass_by_val_and_ref(int num1, int& num2)
+{
+    num1 = 20;
+    num2 = 50;
 
+}
 
 
 /*
@@ -26,3 +31,16 @@ Try to assign a value to num1, generates error.
 
 @return does not return a value
 */
+
+void pass_by_const_ref(const int& num)
+{
+    //num = 50;
+}
+
+void sample_static_var()
+{
+    static int cnt = 0;
+    cnt++;
+    std::cout<<cnt<<"\n";
+}
+
