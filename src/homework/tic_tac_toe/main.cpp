7f9bc90cc529ba5112a1d9454678219bae2009c9
blacksea2003 <vaibhavkrishnaswamy@gmail.com>
2020-10-24 22:49:21 -0500

    homework

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,22 @@
+#include "tic_tac_toe.h"
+#include<iostream>
+
+using std::cout; using std::cin;
 
 int main() 
 {
-
+	tic_tac_toe p;
+	std::string player;
+	cout<<"Enter 'X' or 'O': ";
+	cin>>player;
+	p.start_game(player);
+	int num;
+	while(!p.game_over())
+	{
+		cout<<"enter a number from 1-9: ";
+		cin>>num;
+		p.mark_board(num);
+		p.display_board();
+	}
 	return 0;
 }
--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,50 @@
 //cpp
+#include "tic_tac_toe.h"
+using namespace std;
+bool tic_tac_toe::game_over()
+{
+    return check_board_full();
+}
+
+void tic_tac_toe::start_game(string first_player) {
+    player = first_player;
+    clear_board();
+}
+
+void tic_tac_toe::mark_board(int position){
+    pegs[position-1] == player;
+    set_next_player();
+}
+
+string tic_tac_toe::get_player() const{
+    return player;
+}
+
+void tic_tac_toe::display_board() const {
+        cout<<"["<<pegs[1]<<"]["<<pegs[2]<<"]["<<pegs[3]<<"]\n";
+        cout<<"["<<pegs[4]<<"]["<<pegs[5]<<"]["<<pegs[6]<<"]\n";
+        cout<<"["<<pegs[7]<<"]["<<pegs[8]<<"]["<<pegs[9]<<"]\n";
+}
+
+void tic_tac_toe::set_next_player() {
+    if(player == "X") {
+        player = "O";
+    } else {
+        player = "X";
+    }
+}
+
+bool tic_tac_toe::check_board_full() {
+    for(int i =0 ; i < 9; i++) {
+        if(pegs[i] == " ") {
+            return false;
+        }
+    }
+    return true;
+}
+
+void tic_tac_toe::clear_board() {
+    for(int i =0 ; i < 9; i++) {
+        pegs[i] = " ";
+    }
+}
